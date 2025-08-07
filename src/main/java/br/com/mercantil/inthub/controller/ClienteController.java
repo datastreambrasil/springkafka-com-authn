75ba54e810d536474532492a8530033069fa4534
Ronaldo Lanhellas <dsbr_ronaldo.segundo@mercantil.com.br>
2025-08-07 11:48:59 -0300

    first commit

--- src/main/java/br/com/mercantil/inthub/controller/ClienteController.java
+++ src/main/java/br/com/mercantil/inthub/controller/ClienteController.java
@@ -2,6 +2,10 @@ package br.com.mercantil.inthub.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mercantil.inthub.kafka.Producer;
+import br.com.mercantil.inthub.model.Cliente;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -9,10 +13,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/api")
 public class ClienteController {
 
+    private Producer producer;
+
+    public ClienteController(Producer producer){
+        this.producer = producer;
+    }
+
     
     @GetMapping("/produce")
     public void produce(){
-            
+        producer.produce(new Cliente("Ronaldo", "Sao caetano do sul - SP"));
     }
 
 }
